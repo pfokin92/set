a6461807fe668ca2afbf6afb810f8f3bc88ba268
Pavel Fokin <fokinpavel2007@outlook.com>
2022-01-15 18:50:52 +0300

    first commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,28 @@
+﻿
+import Team from "../app";
+
+const team = new Team();
+
+
+test('Add char first time', ()=>{
+    const team = new Team();
+    team.add('swordsman')
+    expect(team.toArray()).toEqual(['swordsman'])
+});
+
+test('Add char second time', ()=>{
+    const team = new Team();
+    team.add('swordsman');
+    expect(team.add('swordsman')).toThrowError(new Error('Объект уже существует в команде'))
+});
+
+test('Add all chars', ()=>{
+    const team = new Team();
+    team.addAll('swordsman','deamon','bowman')
+    expect(team.toArray()).toEqual(['swordsman','deamon','bowman'])
+});
+
+test('Add all chars double', ()=>{
+    const team = new Team();
+    expect(team.addAll('swordsman','deamon','bowman','swordsman')).toThrowError(new Error('Объект уже существует в команде'))
+});
---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+﻿export default class Team {
+    constructor(){
+      this.members = new Set();
+    }
+    add(name) {  
+      this.members.forEach((item) => {
+        if (item === name) {
+          throw new Error('Объект уже существует в команде');
+        }
+      });
+      this.members.add(name);
+    };
+  
+    addAll(...name){
+      name.forEach((item)=>{
+        this.add(item);
+      });
+    }
+  
+    toArray(){
+      return Array.from(this.members);
+    }
+  }
